f7cff863e020a50b7422f564b03481235191ae27
Jhansi Manikonda <23pa1a4586@vishnu.edu.in>
2026-05-16 11:20:01 +0530

    Update 01_Basic_Queries.sql

--- week1/day1/01_Basic_Queries.sql
+++ week1/day1/01_Basic_Queries.sql
@@ -1,1 +1,22 @@
+-- 1. Select all columns from Employee table
+SELECT * FROM Employee;
 
+-- 2. Select only name and salary columns
+SELECT name, salary
+FROM Employee;
+
+-- 3. Employees older than 30
+SELECT *
+FROM Employee
+WHERE age > 30;
+
+-- 4. Select all department names
+SELECT name
+FROM Department;
+
+-- 5. Employees working in IT department
+SELECT e.*
+FROM Employee e
+JOIN Department d
+ON e.dept_id = d.department_id
+WHERE d.name = 'IT';
