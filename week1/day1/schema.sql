bba7cba321715025e7ffdea6fdd0bcfba971fb34
Jhansi Manikonda <23pa1a4586@vishnu.edu.in>
2026-05-17 23:37:43 +0530

    Create schema.sql

---
+++ week1/day1/schema.sql
@@ -0,0 +1,40 @@
+---
+
+**Query #1**
+
+    SELECT * FROM Employee;
+
+| emp_id | name       | age | salary | dept_id | hire_date  |
+| ------ | ---------- | --- | ------ | ------- | ---------- |
+| 1      | Jhon Doe   | 28  | 50000  | 1       | 2020-01-15 |
+| 2      | Jane Smith | 34  | 60000  | 2       | 2019-07-23 |
+| 3      | Bob Brown  | 45  | 80000  | 1       | 2018-02-12 |
+| 4      | Alice Blue | 25  | 45000  | 3       | 2021-03-22 |
+| 5      | Charlie p  | 29  | 50000  | 2       | 2019-12-01 |
+
+---
+**Query #2**
+
+    SELECT * FROM Department;
+
+| department_id | name      |
+| ------------- | --------- |
+| 1             | IT        |
+| 2             | HR        |
+| 3             | Finance   |
+| 4             | Marketing |
+
+---
+**Query #3**
+
+    SELECT * FROM Project;
+
+| project_id | name            | department_id |
+| ---------- | --------------- | ------------- |
+| 1          | Project Alpha   | 1             |
+| 2          | Project Beta    | 2             |
+| 3          | Project Gamma   | 1             |
+| 4          | Project Delta   | 3             |
+| 5          | Project Epsilon | 4             |
+
+---
