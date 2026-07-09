5fa045b74ba2a2f735ea8e451f8749375cf2d541
Jhansi Manikonda <23pa1a4586@vishnu.edu.in>
2026-07-09 12:31:45 +0530

    Update regexassignment.sql

--- Week 2/regexassignment.sql
+++ Week 2/regexassignment.sql
@@ -1,2 +1,58 @@
-429: Too Many Requests
-For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
+/*
+REGEX Assignment Practice
+*/
+
+-- Q1 Extract numeric characters from beginning
+SELECT REGEXP_SUBSTR(mixed_value,'^[0-9]+')
+FROM regex_practice;
+
+-- Q2 Extract numeric characters from end
+SELECT REGEXP_SUBSTR(mixed_value,'[0-9]+$')
+FROM regex_practice;
+
+-- Q3 Extract first single character
+SELECT REGEXP_SUBSTR(mixed_value,'^.')
+FROM regex_practice;
+
+-- Q4 Extract last single character
+SELECT REGEXP_SUBSTR(mixed_value,'.$')
+FROM regex_practice;
+
+-- Q5 Extract exactly two digits
+SELECT REGEXP_SUBSTR(mixed_value,'[0-9]{2}')
+FROM regex_practice;
+
+-- Q6 Extract one numeric character
+SELECT REGEXP_SUBSTR(mixed_value,'[0-9]')
+FROM regex_practice;
+
+-- Q7 Extract country code from phone number
+SELECT REGEXP_SUBSTR(phone,'[0-9]+')
+FROM regex_practice;
+
+-- Q8 Extract numbers between alphabets
+SELECT REGEXP_SUBSTR(mixed_value,'[0-9]+')
+FROM regex_practice;
+
+-- Q9 Extract username before @
+SELECT REGEXP_SUBSTR(email,'^[A-Za-z0-9._-]+')
+FROM regex_practice;
+
+-- Q10 Extract domain after @
+SELECT REGEXP_SUBSTR(email,'@[A-Za-z.]+')
+FROM regex_practice;
+
+-- Q11 
+Extract email extension
+SELECT REGEXP_SUBSTR(email,'\\.[A-Za-z]+$')
+FROM regex_practice;
+
+-- Q12  
+Extract domain name without @
+SELECTREPLACE(
+REGEXP_SUBSTR(email,'@[A-Za-z.]+'),
+'@',
+''
+)
+FROM regex_practice;
+
